7ada10cb6adba0c0c637715769999357d7786074
CCr0011 <98602237+CCr0011@users.noreply.github.com>
2025-09-20 17:39:04 -0500

    The screen to reset the user's password was created

--- flutter_app/lib/routes/app_routes.dart
+++ flutter_app/lib/routes/app_routes.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../presentation/login/login_page.dart';
 import '../presentation/register/register_page.dart';
-//import '../presentation/forgot_password/forgot_password.dart';
+import '../presentation/forgot_password/forgot_password.dart';
 import '../presentation/feed/feed_page.dart';
 import '../presentation/lost_report/lost_report_page.dart';
 import '../presentation/found_report/found_report_page.dart';
@@ -22,7 +22,8 @@ class AppRoutes {
     return {
       login: (context) => const LoginPage(),
       //register: (context) => const RegisterPage(),
-      //forgot_password: (context) => const ForgotPasswordPage(),
+      forgot_password: (context) => const ForgotPasswordPage(),
+      
       //feed: (context) => const FeedPage(),
       //lostReport: (context) => const LostReportPage(),
       //foundReport: (context) => const FoundReportPage(),
